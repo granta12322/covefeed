a13824a85cfe5084f3ade175610a2814e2ea2181
granta12322 <granta12322@gmail.com>
2022-11-12 11:25:59 +0000

    install typescript

--- pages/DrinkDiary/index.ts
+++ pages/DrinkDiary/index.ts
@@ -1,3 +1,3 @@
 import React from "react";
 
-export function index
+//export function index
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,25 +6,25 @@ import React from 'react'
 import { useState } from 'react'
 import { DrinkItem } from './DrinkDiary/DrinkItem/DrinkItem'
 export default function Home() { 
-  var [navBarShown,setNavBarSown] = useState(false);
+  //var [navBarShown,setNavBarSown] = useState(false);
 
-  var [settings,setSettings] = useState({}); // Get settings from database.
-  var [page, setPage] = useState({})
+  //var [settings,setSettings] = useState({}); // Get settings from database.
+  //var [page, setPage] = useState({})
   return (
 <>
 
 <head>
 
 </head>
-
-<body>
-  <DrinkItem/>
-  <div className='topBar'>
-    <button className='navToggle'></button>
-    <img className='logo'/>
-    <button className='submitDrink'></button>
-  </div>
-  <div className='page'></div>
-</body>
+<body><h1 className="text-3xl font-bold underline">HEllo world!</h1></body>
 </>
 )}
+
+//<DrinkItem/>
+//<div className='topBar'>
+//  <button className='navToggle'></button>
+//  <img className='logo'/>
+//  <button className='submitDrink'></button>
+//</div>
+//<div className='page'></div>
+//</body>
